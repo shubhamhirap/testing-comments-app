2638bef245dacfca5ce01638436a7f4bba06fd23
shubham hirap <shirap07@gmail.com>
2021-01-19 18:51:40 +0530

    written tests for comment box

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
-const CommentBox = () => {
-  return <div>CommentBox</div>;
-};
+class CommentBox extends Component {
+  state = { comment: "" };
+
+  handleChange = (e) => {
+    this.setState({ comment: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({ comment: "" });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a Comment</h4>
+        <textarea value={this.state.comment} onChange={this.handleChange} />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
+}
 
 export default CommentBox;
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,9 +4,13 @@ import App from "components/App";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
-it("shows a comment box", () => {
-  const wrapped = shallow(<App />);
+let wrapped;
 
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
+it("shows a comment box", () => {
   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
